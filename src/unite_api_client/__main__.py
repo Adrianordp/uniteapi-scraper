6cd15e24b2ade8e1fe69e60416171e42bc04b542
Adriano Rodrigues <adrianordp1@gmal.com>
2024-12-07 19:26:44 -0400

    refactor[__main__.py]: use DatabaseClient in entry point

--- src/unite_api_client/__main__.py
+++ src/unite_api_client/__main__.py
@@ -3,12 +3,21 @@ from asyncio import create_task, gather, run
 
 from httpx import AsyncClient
 
+from unite_api_client.db_client import DatabaseClient
 from unite_api_client.unite_api_client import UniteAPIClient
 
 
 async def uniteapi_client():
     api = UniteAPIClient()
     async with AsyncClient() as session:
+        check_last_update_details = create_task(
+            api.check_last_update_details(session)
+        )
+        (date, number_of_games) = await check_last_update_details
+
+        if not api.register_new_table(date, number_of_games):
+            return
+
         task = create_task(api.update_pokemon_list(session))
         await task
 
@@ -19,19 +28,14 @@ async def uniteapi_client():
             ]
         )
         await result
-    api.save_build_win_rate()
-    api.save_build_pick_rate()
-    api.save_pokemon_name()
-    api.save_pokemon_win_rate()
-    api.save_pokemon_pick_rate()
-    api.save_build_win_rate25()
+    api.save_builds_to_database()
 
 
 def main():
-    start = timeit.default_timer()
     run(uniteapi_client())
-    stop = timeit.default_timer()
-    print("Time: ", stop - start)
+    db = DatabaseClient()
+    db._load_all_builds()
+    db.print_by_build_win_rate25()
 
 
 if __name__ == "__main__":

80a5a744298ca680e981777c5c6d2f9736a9d099
Adriano Rodrigues <adrianordp1@gmal.com>
2024-12-07 12:28:21 -0400

    feat[pokemon.py]: unhappy paths for setters

--- src/unite_api_client/pokemon.py
+++ src/unite_api_client/pokemon.py
@@ -31,16 +31,26 @@ class Pokemon:
         return self.name
 
     def set_role(self, role: Role):
+        if not isinstance(role, Role):
+            raise ValueError(f"Invalid role: {role}")
         self.role = role
 
     def set_pick_rate(self, pick_rate: float):
+        if pick_rate < 0 or pick_rate > 100:
+            raise ValueError(f"Invalid pick rate: {pick_rate}")
         self.pick_rate = pick_rate
 
     def set_win_rate(self, win_rate: float):
+        if win_rate < 0 or win_rate > 100:
+            raise ValueError(f"Invalid win rate: {win_rate}")
         self.win_rate = win_rate
 
     def add_move_1(self, move_1: str):
+        if not isinstance(move_1, str):
+            raise TypeError(f"Invalid move_1 type: {type(move_1)}")
         self.moves_1.add(move_1)
 
     def add_move_2(self, move_2: str):
+        if not isinstance(move_2, str):
+            raise TypeError(f"Invalid move_2 type: {type(move_2)}")
         self.moves_2.add(move_2)

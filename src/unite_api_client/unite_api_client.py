d5529f3d5af3266579a20b5fa3f1d9b4502517fa
Adriano Rodrigues <adrianordp1@gmal.com>
2024-12-07 19:25:26 -0400

    refactor[unite_api_client;py]:api stores data in database

--- src/unite_api_client/unite_api_client.py
+++ src/unite_api_client/unite_api_client.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from httpx import AsyncClient
 
 from unite_api_client.build import Build
+from unite_api_client.handle_database import HandleDatabase
 from unite_api_client.pokemon import Pokemon
 
 
@@ -16,6 +17,21 @@ class UniteAPIClient:
         self.route_pokemon_meta_url = self.meta_url + self.route_pokemon_meta
         self.pokemons: list[Pokemon] = [Pokemon] * 0
         self.builds: list[Build] = [Build] * 0
+        self.handle_database = HandleDatabase()
+        self.table_name: str = None
+
+    async def check_last_update_details(self, session: AsyncClient):
+        response = await session.get(self.meta_url, follow_redirects=True)
+        soup = BeautifulSoup(response.content, "html.parser")
+        date = soup.select(
+            "div:nth-child(1) > p.mantine-focus-auto.simpleStat_count__dG_xB.m_b6d8b162.mantine-Text-root"
+        )[0].get_text()
+        number_of_games = int(
+            soup.select(
+                "div:nth-child(2) > p.mantine-focus-auto.simpleStat_count__dG_xB.m_b6d8b162.mantine-Text-root"
+            )[0].get_text()
+        )
+        return (date, number_of_games)
 
     async def update_pokemon_list(self, session: AsyncClient):
         response = await session.get(self.meta_url, follow_redirects=True)
@@ -133,103 +149,15 @@ class UniteAPIClient:
                 )
                 self.builds.append(build_obj)
 
-    def print_by_build_win_rate(self):
-        print("\nSorted by win rate")
-        self.builds.sort(reverse=True)
+    def save_builds_to_database(self):
+        self.handle_database.create_table(self.table_name)
         for build in self.builds:
-            print(build)
+            self.handle_database.insert_build(build)
 
-    def save_build_win_rate25(self):
-        threshold = self.get_pick_rate_threshold()
-        self.builds.sort(reverse=True)
-        with open("log/builds_by_build_win_rate25.log", "w") as f:
-            for build in self.builds:
-                if build.pick_rate < threshold:
-                    continue
-                f.write(str(build) + "\n")
-
-    def save_build_win_rate(self):
-        self.builds.sort(reverse=True)
-        with open("log/builds_by_build_win_rate.log", "w") as f:
-            for build in self.builds:
-                f.write(str(build) + "\n")
-
-    def print_by_build_pick_rate(self):
-        print("\nSorted by pick rate")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.m1m2i_pick_rate, reverse=True)
-        for build in self.builds:
-            print(build)
-
-    def save_build_pick_rate(self):
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pick_rate, reverse=True)
-        with open("log/builds_by_build_pick_rate.log", "w") as f:
-            for build in self.builds:
-                f.write(str(build) + "\n")
-
-    def print_by_pokemon_name(self):
-        print("\nSorted by pokemon")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pokemon.name)
-        for build in self.builds:
-            print(build)
-
-    def save_pokemon_name(self):
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pokemon.name)
-        with open("log/builds_by_pokemon_name.log", "w") as f:
-            for build in self.builds:
-                f.write(str(build) + "\n")
-
-    def print_by_item(self):
-        print("\nSorted by item")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.item)
-        for build in self.builds:
-            print(build)
-
-    def save_item(self):
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.item)
-        with open("log/builds_by_item.log", "w") as f:
-            for build in self.builds:
-                f.write(str(build) + "\n")
-
-    def print_by_pokemon_win_rate(self):
-        print("\nSorted by pokemon win rate")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pkm_win_rate, reverse=True)
-        for build in self.builds:
-            print(build)
-
-    def save_pokemon_win_rate(self):
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pkm_win_rate, reverse=True)
-        with open("log/builds_by_pokemon_win_rate.log", "w") as f:
-            for build in self.builds:
-                f.write(str(build) + "\n")
-
-    def print_by_pokemon_pick_rate(self):
-        print("\nSorted by pokemon pick rate")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pkm_pick_rate, reverse=True)
-        for build in self.builds:
-            print(build)
-
-    def save_pokemon_pick_rate(self):
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pkm_pick_rate, reverse=True)
-        with open("log/builds_by_pokemon_pick_rate.log", "w") as f:
-            for build in self.builds:
-                f.write(str(build) + "\n")
-
-    def get_pick_rate_threshold(self):
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pick_rate, reverse=True)
-        pr_list = []
-        for build in self.builds:
-            pr_list.append(build.pick_rate)
-        quant = quantiles(pr_list)
-        print(f"Quantiles are {quant}")
-        return quant[-1]
+    def register_new_table(self, date, number_of_games):
+        self.table_name = f"{date}_{number_of_games}".replace(" ", "_")
+        if not self.handle_database.register_new_table(
+            date, number_of_games, self.table_name
+        ):
+            return False
+        return True

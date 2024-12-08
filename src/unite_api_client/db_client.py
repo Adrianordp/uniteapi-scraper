3ad658c6bb37757f6a1269f1603c7d2dd226ff4c
Adriano Rodrigues <adrianordp1@gmal.com>
2024-12-07 22:55:30 -0400

    refactor[db_client.py]: improve print of pokemon category

--- src/unite_api_client/db_client.py
+++ src/unite_api_client/db_client.py
@@ -25,6 +25,64 @@ class DatabaseClient:
         print(f"Quantiles are {quant}")
         return quant[-1]
 
+    def print_by_pokemon_name(self):
+        print("\nSorted by pokemon")
+        self.builds.sort(reverse=True)
+        self.builds.sort(key=lambda x: x.pokemon.name)
+        past_pokemon = set()
+        for build in self.builds:
+            if build.pokemon.name in past_pokemon:
+                continue
+            else:
+                past_pokemon.add(build.pokemon.name)
+            string = (
+                f"pkm: {build.pokemon}, "
+                f"WR: {build.pkm_win_rate} %, "
+                f"PR: {build.pkm_pick_rate} %"
+            )
+            print(string)
+
+    def print_by_pokemon_win_rate(self):
+        print("\nSorted by pokemon win rate")
+        self.builds.sort(reverse=True)
+        self.builds.sort(key=lambda x: x.pkm_win_rate, reverse=True)
+        past_pokemon = set()
+        for build in self.builds:
+            if build.pokemon.name in past_pokemon:
+                continue
+            else:
+                past_pokemon.add(build.pokemon.name)
+            string = (
+                f"pkm: {build.pokemon}, "
+                f"WR: {build.pkm_win_rate} %, "
+                f"PR: {build.pkm_pick_rate} %"
+            )
+            print(string)
+
+    def print_by_pokemon_pick_rate(self):
+        print("\nSorted by pokemon pick rate")
+        self.builds.sort(reverse=True)
+        self.builds.sort(key=lambda x: x.pkm_pick_rate, reverse=True)
+        past_pokemon = set()
+        for build in self.builds:
+            if build.pokemon.name in past_pokemon:
+                continue
+            else:
+                past_pokemon.add(build.pokemon.name)
+            string = (
+                f"pkm: {build.pokemon}, "
+                f"WR: {build.pkm_win_rate} %, "
+                f"PR: {build.pkm_pick_rate} %"
+            )
+            print(string)
+
+    def print_by_build_pokemon_name(self):
+        print("\nSorted build by pokemon name")
+        self.builds.sort(reverse=True)
+        self.builds.sort(key=lambda x: x.pokemon.name)
+        for build in self.builds:
+            print(build)
+
     def print_by_build_win_rate25(self):
         threshold = self._get_pick_rate_threshold()
         self.builds.sort(reverse=True)
@@ -38,13 +96,6 @@ class DatabaseClient:
         for build in self.builds:
             print(build)
 
-    def print_by_pokemon_pick_rate(self):
-        print("\nSorted by pokemon pick rate")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pkm_pick_rate, reverse=True)
-        for build in self.builds:
-            print(build)
-
     def print_by_build_pick_rate(self):
         print("\nSorted by pick rate")
         self.builds.sort(reverse=True)
@@ -52,23 +103,9 @@ class DatabaseClient:
         for build in self.builds:
             print(build)
 
-    def print_by_pokemon_name(self):
-        print("\nSorted by pokemon")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pokemon.name)
-        for build in self.builds:
-            print(build)
-
     def print_by_item(self):
         print("\nSorted by item")
         self.builds.sort(reverse=True)
         self.builds.sort(key=lambda x: x.item)
-        for build in self.builds:
-            print(build)
-
-    def print_by_pokemon_win_rate(self):
-        print("\nSorted by pokemon win rate")
-        self.builds.sort(reverse=True)
-        self.builds.sort(key=lambda x: x.pkm_win_rate, reverse=True)
         for build in self.builds:
             print(build)
